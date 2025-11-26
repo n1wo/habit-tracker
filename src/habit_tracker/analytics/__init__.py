436e61b96546c73b2e05a628db594e562e7d97b9
n1wo <n.wokurka@protonmail.com>
2025-11-26 21:27:38 +0100

    ✨ feat(analytics): implement habit analytics functions and unit tests for streak calculations

---
+++ src/habit_tracker/analytics/__init__.py
@@ -0,0 +1,15 @@
+from .analytics import (
+    list_all_habits,
+    list_habits_by_periodicity,
+    calculate_streak,
+    longest_streak_overall,
+    longest_streak_by_habit,
+)
+
+__all__ = [
+    "list_all_habits",
+    "list_habits_by_periodicity",
+    "calculate_streak",
+    "longest_streak_overall",
+    "longest_streak_by_habit",
+]
